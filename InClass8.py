f3125027ab9eb0af10316ff28a980fa6538435d2
David <drobinson12@student.waubonsee.edu>
2019-03-14 11:51:22 -0500

    Changes added: A while loop to keep all numbers entered within 0 - 1000, and some clean up.

--- InClass8.py
+++ InClass8.py
@@ -1,16 +1,56 @@
 
+num1Prob = True
+num2Prob = True
+num3Prob = True
+num4Prob = True
+num5Prob = True
 
-num1 = int(input("Please enter the first number"))
+while num1Prob == True:
 
-num2 = int(input("Pleasee enter the second number"))
+    num1 = int(input("Please enter the first number :"))
 
-num3 = int(input("PLease enter the third number"))
+    if num1 >= 0 and num1 <= 1000:
+        num1Prob =False
+    else:
+        print ("Error num not in Range")
 
-num4 = int(input("please enter the fourth number"))
+while num2Prob == True:
+    num2 = int(input("Pleasee enter the second number :"))
 
-num5 = int(input("Please enter the fifth number"))
+    if num2 >= 0 and num2 <=1000:
+        num2Prob = False
+    else:
+        print ("Error num not in Range")
 
 
+while num3Prob == True: 
+    num3 = int(input("PLease enter the third number :"))
+
+    if num3 >= 0 and num2 <= 1000:
+        num3Prob = False
+    else:
+        print ("Error num not in Range") 
+        
+    
+while num4Prob == True:
+    num4 = int(input("please enter the fourth number :"))
+        
+    if num4 >= 0 and num4 <= 1000:
+        num4Prob = False
+    else:
+        print ("Error num not in Range")    
+
+
+while num5Prob == True:
+    num5 = int(input("Please enter the fifth number :"))
+
+    if num5 >= 0 and num5 <= 1000:
+        num5Prob = False
+    else:
+        print ("Error num not in Range")    
+
+
+## for lowest
 if num1 < num2 and num1 < num3 and num1 < num4 and num1 < num5:
 
     print ("The smallest number is ", num1)
@@ -33,7 +73,7 @@ else:
 
 
 
-## for the lowest
+## for the largest
 
 if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5:
 
@@ -53,3 +93,7 @@ elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5:
 
 else:
     print ("the largest number is", num5)
+
+
+
+
